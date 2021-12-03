ac23fe063166f6813f9a75a77ccc74bb13e94b87
Josh Hendricks <jhendricks123@gmail.com>
2021-12-02 16:47:56 -0800

    Add random deviations and additional properties

--- simulate.ps1
+++ simulate.ps1
@@ -1,4 +1,4 @@
-function Test-LogServerBehavior {
+function Invoke-LogServerSimulator {
     [CmdletBinding()]
     param (
         # Specifies the number of new log messages generated per day
@@ -32,7 +32,14 @@ function Test-LogServerBehavior {
         [Parameter()]
         [ValidateRange(1, [int]::MaxValue)]
         [int]
-        $ExpiredLogsMaximumLifetimeInDays = 7
+        $ExpiredLogsMaximumLifetimeInDays = 7,
+
+        # Add or subtract a random number of daily log records for a more realistic simulation.
+        # Expressed as a percentage value where 0.2 means up to 20% deviation of the daily logs per day
+        [Parameter()]
+        [ValidateRange(0,1)]
+        [double]
+        $MaxDeviation = 0
     )
     
     process {
@@ -41,25 +48,42 @@ function Test-LogServerBehavior {
         $swap = New-Object system.collections.generic.list[int]
         $lastTableSwap = 1
         $nextTableSwap = $lastTableSwap + $ExpiredLogsMaximumLifetimeInDays
+        
         for ($day = 1; $day -le $SimulationDuration; $day++) {
-            1..$LogsPerDay | Foreach-Object {
+            # Add new logs to the active table
+            $logsToCreateToday = $LogsPerDay
+            if ($MaxDeviation -ne 0) {
+                $deviation = Get-Random -Minimum 0 -Maximum ([int]($LogsPerDay * $MaxDeviation))
+                if ($deviation % 2) {
+                    $deviation *= -1
+                }
+                $logsToCreateToday += $deviation
+            }
+            1..$logsToCreateToday | Foreach-Object {
                 $active.Add($day)
             }
 
+            # Calculate the % not expired
             $percentNotExpired = 100
             $countNotExpired = 0
+            $countExpired = 0
             $cutoffDay = $day - $LogRetention
             if ($inactive.Count -gt 0) {
                 for ($i = 0; $i -lt $inactive.Count; $i++) {
-                    if ($inactive[$i] -ge $cutoffDay) {
-                        $countNotExpired++
+                    if ($inactive[$i] -lt $cutoffDay) {
+                        $countExpired++
+                    } else {
+                        break
                     }
                 }
+                $countNotExpired = $inactive.Count - $countExpired
                 $percentNotExpired = [int]($countNotExpired / $inactive.Count * 100)
             }
 
+            # Simulate table swap if unexpired logs is less than or equal to the threshold or the max days between swaps is reached
             $rowsCopied = 0
             $tablesSwapped = $false
+            $tableSwapReason = $null
             if ($percentNotExpired -le $LogDeleteThreshold -or $day - $lastTableSwap -ge $ExpiredLogsMaximumLifetimeInDays) {
                 $swap = $inactive | Where-Object { $_ -gt $cutoffDay}
                 $rowsCopied = $swap.Count
@@ -68,54 +92,71 @@ function Test-LogServerBehavior {
                 $swap | Foreach-Object { $active.Add($_) }
                 $lastTableSwap = $day
                 $nextTableSwap = $lastTableSwap + $ExpiredLogsMaximumLifetimeInDays
+                $tablesSwapped = $true
+                if ($percentNotExpired -le $LogDeleteThreshold) {
+                    $tableSwapReason = 'LogDeleteThreshold'
+                } else {
+                    $tableSwapReason = 'ExpiredLogsMaximumLifetimeInDays'
+                }
             }
 
+            # Output the daily statistics
             [PSCustomObject]@{
                 Day = $day
                 ActiveTableSize = $active.Count
                 InactiveTableSize = $inactive.Count
+                TotalRecords = $active.Count + $inactive.Count
                 PercentNotExpired = $percentNotExpired
                 TableSwapOccurred = $tablesSwapped
+                TableSwapTrigger = $tableSwapReason
                 RowsCopied = $rowsCopied
                 NextTableSwap = $nextTableSwap
-            }            
+                OldestActiveRecord = if ($active.Count -gt 0) { $day - $active[0] } else { 0 }
+                OldestInactiveRecord = if ($inactive.Count -gt 0) { $day - $inactive[0] } else { 0 }
+                NewestInactiveRecord = if ($inactive.Count -gt 0) { $day - $inactive[-1] } else { 0 }
+            } 
         }
     }
 }
 
 <#
-    Assumptions
-    •	5% threshold (which we did set)
-    •	The weekly procedure is stopped, table swaps only happen at the 5% threshold.
-    •	104,150 new records per day (based off of your average rate over the last year)
-    •	Expiration policy of 365 days (which is the current policy).
+    Sample Test Case:
+    - 104,150 records per day
+    - 365 day retention
+    - 5% threshold or 1% threshold
 #>
 
-Clear-Host
+# This defines the settings for the simulation
 $settings = @{
-    LogsPerDay = 104150
-    LogRetention = 365
-    SimulationDuration = 365 * 3
-    LogDeleteThreshold = 5
-    ExpiredLogsMaximumLifetimeInDays = 366
-}
-
-$testSettings = @{
     LogsPerDay = 100
-    LogRetention = 30
-    SimulationDuration = 365
-    LogDeleteThreshold = 20
-    ExpiredLogsMaximumLifetimeInDays = 7
+    LogRetention = 365
+
+    # Specified in Days.
+    SimulationDuration = 365 * 3
+    
+    # The percentage of records in the inactive table that are NOT expired
+    # A table swap is triggered when the percentage of NOT expired records
+    # is less than, or equal to this value.
+    LogDeleteThreshold = 1
+
+    # The maximum number of days before forcing a table swap even if
+    # LogDeleteThreshold is not yet reached.
+    ExpiredLogsMaximumLifetimeInDays = 366
+
+    # Adds a random daily deviation for LogsPerDay of up to +-5%
+    MaxDeviation = 0.05
 }
 
-$results = new-object system.collections.generic.list[pscustomobject]
-Test-LogServerBehavior @testSettings | Foreach-Object {
-    $results.Add($_)
+# Create file name based on settings and remove the previous CSV file if present
+$fileName = 'TableSwap-Simulation_{0}-logs-per-day_{1}-days-retention_{2}-threshold_{3}-max-days-between-swaps.csv' -f $settings.LogsPerDay, $settings.LogRetention, $settings.LogDeleteThreshold, $settings.ExpiredLogsMaximumLifetimeInDays
+$path = Join-Path $PSScriptRoot $fileName
+if (Test-Path $path) {
+    Remove-Item -Path $path
+}
+
+# Run the simulator with the settings in the $settings variable above
+# and simultaneously export each row to CSV and display it in Out-GridView
+Invoke-LogServerSimulator @settings | Foreach-Object {
+    $_ | Export-Csv -Path $path -NoTypeInformation -Append
     $_
 } | Out-GridView
-
-$settings = $testSettings
-$fileName = 'TableSwap-Simulation_{0}-logs-per-day_{1}-days-retention_{2}-threshold_{3}-max-days-between-swaps.csv' -f $settings.LogsPerDay, $settings.LogRetention, $settings.LogDeleteThreshold, $settings.ExpiredLogsMaximumLifetimeInDays
-$results | Export-Csv -Path $PSScriptRoot/$fileName -NoTypeInformation -Force
-
-#Test-LogServerBehavior -LogsPerDay 104150 -LogRetention 30 -SimulationDuration 60 | Format-Table
